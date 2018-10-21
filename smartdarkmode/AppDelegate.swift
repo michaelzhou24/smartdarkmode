7805f8426e63524c04524cf10ac76f434bec889a
Michael Zhou <michaelzhou24@gmail.com>
2018-10-21 12:24:27 -0700

    Initial Commit

---
+++ smartdarkmode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  smartdarkmode
+//
+//  Created by Michael Zhou on 2018-10-21.
+//  Copyright © 2018 Michael Zhou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ smartdarkmode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  smartdarkmode
+//
+//  Created by Michael Zhou on 2018-10-21.
+//  Copyright © 2018 Michael Zhou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
